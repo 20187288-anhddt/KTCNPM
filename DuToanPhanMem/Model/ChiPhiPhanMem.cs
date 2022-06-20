96978ba0916dedd562bab061c70f8a2d3218ae27
20187288-anhddt <73302743+20187288-anhddt@users.noreply.github.com>
2022-06-21 01:38:17 +0700

    Add files via upload

---
+++ DuToanPhanMem/Model/ChiPhiPhanMem.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuToanPhanMem.Model
+{
+    public class ChiPhiPhanMem:INotifyPropertyChanged
+    {
+        private long _giaTriPhanMem;
+        private long _chiPhiChung;
+        private long _thue;
+        private long _tong;
+       
+
+        public long GiaTriPhanMem
+        {
+            get { return this._giaTriPhanMem; }
+            set
+            {
+                if (this._giaTriPhanMem != value)
+                {
+                    this._giaTriPhanMem = value;
+                    OnPropertyChanged("GiaTriPhanMem");
+                    TinhChiPhi();
+                }
+                    
+
+            }
+        }
+
+        public long ChiPhiChung {
+            get { return this._chiPhiChung; }
+            set
+            {
+                if (this._chiPhiChung != value)
+                {
+                    this._chiPhiChung = value;
+                    OnPropertyChanged("ChiPhiChung");
+                    TinhChiPhi();
+                }
+
+
+            }
+        }
+
+        public long Thue {
+            get { return this._thue; }
+            set
+            {
+                if (this._thue != value)
+                {
+                    this._thue = value;
+                    OnPropertyChanged("Thue");
+                    TinhChiPhi();
+                }
+
+
+            }
+        }
+
+        public long Tong {
+            get { return this._tong; }
+            set
+            {
+                if (this._tong != value)
+                {
+                    this._tong = value;
+                    OnPropertyChanged("Tong");
+                }
+
+
+            }
+        }
+
+
+        
+
+        public void TinhC()
+        {
+            ChiPhiChung = Convert.ToInt64( 0.65f * GiaTriPhanMem);
+        }
+
+        public void TinhTL()
+        {
+            
+            Thue = Convert.ToInt64( 0.06f * (GiaTriPhanMem + ChiPhiChung));
+        }
+
+        public void TinhChiPhi()
+        {
+            TinhC();
+            TinhTL();
+            Tong = GiaTriPhanMem + ChiPhiChung + Thue;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
